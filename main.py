4fbae3ecfad599dcfd2432893ca93d064c8e0c6d
Patrick Smallwood <patrickjsmallwood@gmail.com>
2017-10-30 19:18:28 -0500

    final commit for user-signup fr

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from flask import Flask, request, redirect, render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+@app.route('/', methods=['GET'])
+def index():
+    return redirect('/signup')
+
 @app.route('/signup', methods=['POST', 'GET'])
 def user_submission():
    
@@ -17,7 +21,10 @@ def user_submission():
         username_error = ''
         password_error = ''
         ver_error = ''
+        email_error = ''
 
+        
+       
         if ' ' in username or username == '':
             username_error = 'invalid username.'
         
@@ -31,20 +38,45 @@ def user_submission():
         if ' ' in password or password == '':
             password_error = 'invalid password.' 
             password = ''
+        
+        elif len(password) > 20 or len(password) < 3:
+                password_error = 'password must be between (4-19) characters.'
+        
         elif verify_password == '':
             ver_error = 'please verify password' 
+        
         else:
             if password != verify_password:
                 ver_error = 'passwords do not match.'
                 password = ''
                 verify_password = ''
-               
+
+    
+
+        if email != '':
+            at_count = 0
+            dot_count = 0
+            if (len(email) > 20 or len(email) < 3):
+                email_error = 'email must be between (4-19) characters.'
+            for i in email:
+                if i == ' ':
+                    email_error = 'email cannot contain spaces.'
+                elif i == '@':
+                    at_count = at_count + 1
+                elif i == '.':
+                    dot_count = dot_count + 1
+            if at_count > 1 or at_count < 1:
+                email_error = 'invalid email.'
+            if dot_count > 1 or dot_count < 1:
+                email_error = 'invalid email.'
+
+
             
-        if not username_error and not password_error and not ver_error:
-            return render_template('welcome.html', username=username)
+        if not username_error and not password_error and not ver_error and not email_error:
+            return render_template('welcome.html', username=username, email=email)
 
         else:  
-            return render_template('signup-form.html', username=username, username_error=username_error, password=password, verify_password=verify_password, password_error=password_error, ver_error=ver_error, email=email)
+            return render_template('signup-form.html', username=username, username_error=username_error, password=password, verify_password=verify_password, password_error=password_error, ver_error=ver_error, email=email, email_error=email_error)
  
     else:
         return render_template('signup-form.html')
